f4380825985e49f9c77856d89b574ffd21bd3b46
Abdulmalik <219211258@psu.edu.sa>
2023-02-18 19:11:19 +0300

    Pushing JenkinsWithGit.java

--- demo222.java
+++ demo222.java
@@ -2,7 +2,6 @@ public class DEMO
 {
     public static void main(String[] args)
     {
-	for(int i=0; i<10; i++)
-		system.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
     }
 }
